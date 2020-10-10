5276fb965194593f93e8f40498c59524feca5888
michaelharootoonyan <harootoonyanmichael@gmail.com>
2020-10-10 10:51:12 -0700

    refactored - and improved ux upon adding custom tab

--- admin/js/jscript.js
+++ admin/js/jscript.js
@@ -1,50 +1,54 @@
-jQuery(document).ready(function($){
-    const $yourCustomAddedTabsList = $('#your-custom-added-tabs');
-    const $wpuDeleteCustomFieldBtns = $('.wpu-delete-custom-field-btn');
-    const n = $wpuDeleteCustomFieldBtns.size();
-    var currentCustomFieldCount = n;
+jQuery(document).ready(function($)
+{
+    const $yourCustomAddedTabsList = $('#your-custom-added-tabs'),
+          $wpuAddCustomTabRequirement = $('#wpu-add-custom-tab-requirement');
+
+    const $wpuDeleteCustomFieldBtns = $yourCustomAddedTabsList.find('.wpu-delete-custom-field-btn');
+
+    let n = $wpuDeleteCustomFieldBtns.size();
 
     const deleteCustomField = function(){
       const $a  = $(this);
       const $li = $a.parent();
       $a.unbind('click');
       $li.fadeOut('slow');
-      currentCustomFieldCount--;
-      if (currentCustomFieldCount == 0){
+      n--;
+      if (n == 0){
         $yourCustomAddedTabsList.hide();
         $yourCustomAddedTabsList.append('<li class="list-group-item none-added">None added!  Use the input field above to add custom tabs..</li>');
         $yourCustomAddedTabsList.fadeIn(2999);
       }
     }
 
-    $wpuDeleteCustomFieldBtns.on('click', deleteCustomField);
+    $wpuDeleteCustomFieldBtns.click(deleteCustomField);
 
-    const $wpuAddCustomTabRequirement = $('#wpu-add-custom-tab-requirement');
     $wpuAddCustomTabRequirement.keypress(function(event) {
-        if (event.keyCode == 13) {
-          var li = document.createElement("li");
-          li.className = "list-group-item";
-          var txtNode = document.createTextNode($(this).val());
-          $(li).append(txtNode);
-          var label = document.createElement('label');
-          label.className="wpu-delete-custom-field-btn";
-          var a = document.createElement('a');
-          $(label).bind('click', deleteCustomField);
-        //   $(a).append('<img src="<?php echo WOOCOMMERCE_PRODUCT_UPLOADER_URL_PATH ?>/public/img/close__.png"></a>');
-          $(a).append('<img src="' + WPU_REMOTE.URL + '/public/img/close__.png"></a>');
-          $(label).append(a);
-          $(li).append(label);
-          $(li).hide();
-          $yourCustomAddedTabsList.append(li);
-          $(li).fadeIn(1999);
-          if (currentCustomFieldCount == 0){
-            $yourCustomAddedTabsList.find('li.list-group-item.none-added').remove();
-          }
-          currentCustomFieldCount++;
-          // $yourCustomAddedTabsList.append('<li class="list-group-item">' + $(this).val() + '<label class="wpu-delete-custom-field-btn"><a><img src="http://localhost/wpu/wp-content/plugins/woocommerce-product-uploader/public/img/close__.png"></a></label></li>')
-                                  // .hide()
-                                  // .fadeIn(1999)
-                                  // .bind('click', deleteCustomField);
+        if (event.keyCode != 13) return;  // soon as they press enter let the code run below
+        
+        const $self = $(this);
+        // appending to the custom tabs list
+        var li = document.createElement("li");
+        li.className = "list-group-item";
+        var txtNode = document.createTextNode($self.val());
+        $self.val('');
+        $(li).append(txtNode);
+        var label = document.createElement('label');
+        label.className="wpu-delete-custom-field-btn";
+        var a = document.createElement('a');
+        $(label).bind('click', deleteCustomField);
+        $(a).append('<img src="' + WPU_REMOTE.URL + '/public/img/close__.png"></a>');
+        $(label).append(a);
+        $(li).append(label);
+
+        // animation of the appending
+        if (n == 0){
+          // remove the "add custom tabs to populate here" notice
+          $yourCustomAddedTabsList.find('li.list-group-item.none-added').fadeOut(999).remove();
         }
+        n++;
+        $(li).hide();
+        $yourCustomAddedTabsList.append(li);
+        $(li).fadeIn(1999);
+
     })
 });
